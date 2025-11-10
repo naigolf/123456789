876708cc2f794dc29769668acbdb98cc1e81d639
nai golf <niy_golf@hotmail.com>
2025-11-10 13:06:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -211,5 +211,8 @@ def upload_files():
 def download_zip(filename):
     return send_file(os.path.join(ZIPPED_FOLDER, filename), as_attachment=True)
 
+# ... โค้ดเดิมทั้งหมด ...
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
